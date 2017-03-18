ef83c710564205d5fa36dd1633f4033f1569e26e
Dick van Hirtum <dvhirtum@gmail.com>
2017-03-18 15:04:56 +0100

    Added simple player object

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	public float Speed;
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+		Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+		rigidBody.velocity = movement * Speed;
+	}
+}
