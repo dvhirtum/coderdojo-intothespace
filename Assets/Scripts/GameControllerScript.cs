2f54e6aff99394e8b3493a30f7e238f56335c657
Dick van Hirtum <dvhirtum@gmail.com>
2017-03-19 17:42:35 +0100

    Created gamecontroller to spawn enemies and asteroid prefab

---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemyType 
+{
+	public GameObject Enemy;
+	public int Count;
+	public float SpawnWait;
+	public float StartWait;
+	public float WaveWait;
+}
+
+public class GameControllerScript : MonoBehaviour {
+	public EnemyType[] Enemies;
+
+	void Start () {
+		foreach (EnemyType enemyType in Enemies) {
+			StartCoroutine(spawnWaves(enemyType));
+		}
+	}
+	
+	void Update () {
+		
+	}
+
+	IEnumerator spawnWaves(EnemyType enemyType)
+	{
+		yield return new WaitForSeconds (enemyType.StartWait);
+
+		while (true)
+		{
+			for (int i = 0; i < enemyType.Count; i++)
+			{
+				Vector2 spawnPosition = new Vector2 (Random.Range (-3, 3), 6);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (enemyType.Enemy, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (enemyType.SpawnWait);
+			}
+			yield return new WaitForSeconds (enemyType.WaveWait);
+		}
+	}
+}
