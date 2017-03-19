c992e446d75474d7e16d695195d8eb6144cd6698
Dick van Hirtum <dvhirtum@gmail.com>
2017-03-19 22:27:32 +0100

    Added blue enemy

---
+++ Assets/Scripts/EnemyBlueScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class EnemyBlueScript : MonoBehaviour 
+{
+	public float Speed;
+	
+	void Start () 
+	{
+		GetComponent<Rigidbody2D>().velocity = -1 * transform.up * Speed;
+	}
+}
